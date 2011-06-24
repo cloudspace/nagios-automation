f06329b1d6aa41220e246df98310dc93100fed31
Josh Lindsey <josh@cloudspace.com>
2011-06-23 20:54:02 -0400

    Revert bc30540239f4806acfe74c43401538adf31583c5^..HEAD

--- lib/generator.rb
+++ lib/generator.rb
@@ -82,7 +82,7 @@ class Generator
     groups = []
 
     os.run_list.each do |item|
-      if /role\[(?<group>.+?)\]/ =~ item
+      if /role\[(?<group>\w+)\]/ =~ item
         groups << group
       end
     end
@@ -110,7 +110,7 @@ class Generator
       RunnerUtils.debug "Parsing runlist item: #{item}"
 
       # Parse item
-      unless /(?<type>.+?)\[(?<name>.+?)\]/ =~ item
+      unless /(?<type>\w+)\[(?<name>\w+)\]/ =~ item
         RunnerUtils.warn "Unable to parse runlist item: #{item}"
         next
       end
