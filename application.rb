57a6bca469ce4ff6763d6d4e79d9b0430759c01c
Josh Lindsey <josh@cloudspace.com>
2011-06-27 09:42:37 -0400

    DRY up the Sinatra application file

--- application.rb
+++ application.rb
@@ -1,47 +1,51 @@
-require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'json'
 require 'resque'
 
-$:.unshift File.expand_path('lib', File.dirname(__FILE__))
-Dir['lib/*.rb'].each { |lib| require File.basename(lib) }
+configure do
+  $:.unshift File.expand_path('lib', File.dirname(__FILE__))
+  Dir['lib/*.rb'].each { |lib| require File.basename(lib) }
+end
+
+before do
+  content_type :json
+  headers 'Connection' => 'close'
+end
+
+get '/' do
+  status 404
+end
 
 post '/register' do
-  content_type 'application/json'
+  out = { :success => true }.to_json
+  code = 200
 
   begin
     parsed = JSON(request.body.read)
     Resque.enqueue(Runner, 'register', parsed)
-   
-    out = { :success => true }.to_json
-
-    status 200
-    body out
   rescue Exception => e
     out = { :success => false, :error => e.to_s }.to_json
-
-    status 500
-    body out
+    code = 500
   end
+
+  status code
+  body out
 end
 
 post '/unregister' do
-  content_type 'application/json'
-  
+  out = { :success => true }.to_json
+  code = 200
+
   begin
     parsed = JSON(request.body.read)
     Resque.enqueue(Runner, 'unregister', parsed)
-    
-    out = { :success => true }.to_json
-
-    status 200
-    body out
   rescue Exception => e
     out = { :success => false, :error => e.to_s }.to_json
-
-    status 500
-    body out
+    code = 500
   end
+  
+  status code
+  body out
 end
 
